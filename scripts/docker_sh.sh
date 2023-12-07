21b6aa9e4b2abebb315d998a480460261e677db7
Sebastian Davids <sdavids@gmx.de>
2023-12-07 11:48:13 +0100

    chore: Go http(s ) server setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker_sh.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-project-template-go'
+
+docker container exec \
+  --interactive \
+  --tty \
+  "${container_name}" \
+  sh
