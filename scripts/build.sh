bee64228782b4be337d0d33c0559ee5c47a01512
Sebastian Davids <sdavids@gmx.de>
2023-12-07 11:48:13 +0100

    chore: Go setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+cd "${base_dir}"
+
+go build -o target/sdavids-project-template cmd/sdavids-project-template.go
---
+++ scripts/lint_fix.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# golangci-lint needs to be in $PATH
+#
+# https://golangci-lint.run/usage/quick-start
+# https://github.com/golangci/golangci-lint/issues/2654#issuecomment-1606439587
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+cd "${base_dir}"
+
+go list -f '{{.Dir}}/...' -m | xargs golangci-lint run --fix
