24aba79a143796594ccb1abfd529cd0324964118
Sebastian Davids <sdavids@gmx.de>
2023-12-07 11:48:13 +0100

    chore: Go setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ cmd/sdavids-project-template.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"log/slog"
+)
+
+func main() {
+	slog.Info("sdavids-project-template")
+}
