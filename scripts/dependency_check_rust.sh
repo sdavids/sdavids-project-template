a02d4a01f4060a74a40dd6d668a1ac1c2a8fa4b4
Sebastian Davids <sdavids@gmx.de>
2023-12-08 12:27:21 +0100

    chore: cargo and rustfmt setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/dependency_check_rust.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# script needs to be invoked from the project's root directory
+
+set -eu
+
+cargo update --aggressive --dry-run
