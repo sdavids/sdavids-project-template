5db5b5ce02a77708758cf12ae2a54b9a059256c1
Sebastian Davids <sdavids@gmx.de>
2023-12-08 12:27:21 +0100

    chore: cargo and rustfmt setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+use std::io::{stderr, stdout, Write};
+
+fn main() {
+  println!("sdavids-project-template");
+  eprintln!("sdavids-project-template");
+  stdout().flush().expect("could not flush stdout");
+  stderr().flush().expect("could not flush stderr");
+}
