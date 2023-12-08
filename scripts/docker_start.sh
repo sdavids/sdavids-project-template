dc4c2b701efb8ac857798cec7c898bf5ddc71ae1
Sebastian Davids <sdavids@gmx.de>
2023-12-08 12:27:21 +0100

    cargo and rustfmt setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker_start.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly tag='local'
+
+# https://docs.docker.com/reference/cli/docker/image/tag/#description
+readonly namespace='de.sdavids'
+readonly repository='sdavids-project-template'
+
+readonly label_group='de.sdavids.docker.group'
+
+readonly label="${label_group}=${repository}"
+
+readonly image_name="${namespace}/${repository}"
+
+readonly container_name='sdavids-project-template-rust'
+
+# to ensure ${label} is set, we use --label "${label}"
+# which might overwrite the label ${label_group} of the image
+docker container run \
+  -a stdout \
+  --rm \
+  --read-only \
+  --user app \
+  --security-opt='no-new-privileges=true' \
+  --cap-drop=all \
+  --network='none' \
+  --name "${container_name}" \
+  --label "${label}" \
+  "${image_name}:${tag}"
