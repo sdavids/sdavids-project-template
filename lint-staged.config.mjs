48c8b4a13867c22ea602fda7cb0d1d98272225b1
Sebastian Davids <sdavids@gmx.de>
2023-12-19 18:42:05 +0100

    JS, HTML, and CSS setup
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ lint-staged.config.mjs
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+// https://github.com/okonet/lint-staged#configuration
+
+/** @type {import('lint-staged').Configuration} */
+export default {
+  "*.{cjs,js,mjs}": ["eslint"],
+  "*.{cjs,css,html,js,json,mjs}": ["prettier --check"],
+  "*.yaml": ["prettier --check", "yamllint --strict"],
+  "*.sh": [
+    "shellcheck",
+    "shfmt --diff --indent 2 --case-indent --binary-next-line --simplify",
+  ],
+  Dockerfile: ["hadolint"],
+};
