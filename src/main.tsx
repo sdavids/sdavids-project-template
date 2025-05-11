54cb4b55c6e926feedb796ad0f200365d3777c9a
Sebastian Davids <sdavids@gmx.de>
2025-05-11 21:32:25 +0200

    add react support
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
 // SPDX-License-Identifier: Apache-2.0
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { assertNonNullish } from "./lib/utils.ts";
 
 import "./main.css";
@@ -8,8 +10,10 @@ import "./main.css";
 const rootElement = document.getElementById("root");
 assertNonNullish("unable to find DOM element #root", rootElement);
 
-rootElement.innerHTML = `
-<main>
-  <h1>sdavids-project-template</h1>
-</main>
-`;
+createRoot(rootElement).render(
+  <StrictMode>
+    <main>
+      <h1>sdavids-project-template</h1>
+    </main>
+  </StrictMode>,
+);
